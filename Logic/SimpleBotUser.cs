e918ca76f4b3f53219407117175ee76520546162
Fabricio Catae <fcatae@microsoft.com>
2018-02-21 00:16:44 -0300

    add user profile

--- Logic/SimpleBotUser.cs
+++ Logic/SimpleBotUser.cs
@@ -11,5 +11,14 @@ namespace SimpleBot
         {
             return $"{message.User} disse '{message.Text}'";
         }
+
+        public static UserProfile GetProfile(string id)
+        {
+            return null;
+        }
+
+        public static void SetProfile(string id, UserProfile profile)
+        {
+        }
     }
 }
---
+++ Logic/UserProfile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleBot
+{
+    public class UserProfile
+    {
+        public string Id { get; set; }
+        public int Visitas { get; set; }
+    }
+}
