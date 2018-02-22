2e813add988a0a405127d40b8e62717117b13abc
Fabricio Catae <fcatae@microsoft.com>
2018-02-22 08:40:48 -0300

    remove namespace reference

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,10 +1,6 @@
-using Autofac;
 using System.Web.Http;
 using System.Configuration;
 using System.Reflection;
-using Microsoft.Bot.Builder.Azure;
-using Microsoft.Bot.Builder.Dialogs;
-using Microsoft.Bot.Builder.Dialogs.Internals;
 using Microsoft.Bot.Connector;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
