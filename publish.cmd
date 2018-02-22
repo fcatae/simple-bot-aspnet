a45aeb09ae5ced00413b99e174a87404f91eff79
Fabricio Catae <fcatae@microsoft.com>
2018-02-22 09:49:28 -0300

    remove unused WebApp files

--- publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=fcataebot1-Web-Deploy.pubxml -p:Password=90ydTQdqMgsl0epiR8kgnaDatk9BX855C3hNm766HPsoJ7wRfvdgtis9dnJB
-
